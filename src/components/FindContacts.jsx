2c730b0960722988f1ce664f52fa90acc3376407
vaj573 <jhonedw573@gmail.com>
2024-02-04 21:06:20 -0500

    elimine patern para que el codigo se cargue sin ningun error para ver si funciona en git hub pages

--- src/components/FindContacts.jsx
+++ src/components/FindContacts.jsx
@@ -6,6 +6,8 @@ const FindContacts = ({handleFilterChange}) => {
         <>
        <h4>Find contacts by name </h4>
             <input type="text"
+                id="find"
+                name="find"
                 placeholder="Find contact"
                  onChange={handleFilterChange} // Agrega el manejador de cambios
     
