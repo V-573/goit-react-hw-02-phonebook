6523ded32df264053c33c3c9ac4e43a5a70a1427
vaj573 <jhonedw573@gmail.com>
2024-02-05 20:56:46 -0500

    CAMBIE EL JASON LA PARTE DEL HOMEPAGE, ESTABA CON EL NOMBRE DEL CREADOR DEL TEMPLATE

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -23,7 +23,7 @@ const Contacts= ( {handleSubmit, handleNameChange, value,value2, handleNumbersCh
                 name="number"
                 value={value2}
                 onChange={handleNumbersChange}
-//   pattern="\+?\d{1,4}[-.\s]?\(?\\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  //pattern="\+?\d{1,4}[-.\s]?\(?\\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
 
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
